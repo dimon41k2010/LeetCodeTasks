5a2e5d01851c95a2d6cca062e31aba848a2b9bab
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2021-10-15 10:57:29 -0700

    Time: 23 ms (96.64%), Space: 14.1 MB (72.92%) - LeetHub

---
+++ jewels-and-stones/jewels-and-stones.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        res = 0
+        set_jewels = set(jewels)
+        for stone in stones:   #O(n)
+            if stone in set_jewels:  #O(1)
+                res += 1
+        return (res)
+
+# Time: O(n + m) m=stones, (O(1) set(n)=jewels)
+# Space: O(n)
