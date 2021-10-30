a1948264c8311efc127da59ef72b7c778c09b7b6
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2021-10-30 10:43:47 -0700

    Time: 28 ms (91.26%), Space: 14.1 MB (80.65%) - LeetHub

--- check-if-numbers-are-ascending-in-a-sentence/check-if-numbers-are-ascending-in-a-sentence.py
+++ check-if-numbers-are-ascending-in-a-sentence/check-if-numbers-are-ascending-in-a-sentence.py
@@ -1,19 +1,26 @@
 class Solution:
     def areNumbersAscending(self, s: str) -> bool:
-        i = 0
-        prev = -1
-        while i < len(s):
-            if s[i].isdigit(): # O(1)
-                start = i
-                while i < len(s) and  s[i].isdigit():  # O(1)
-                    i += 1
-                num = int(s[start:i])
-                if num <= prev:
-                    return (False)
-                prev = num
-            else:
-                i += 1
-        return (True)
+        # i = 0
+        # prev = -1
+        # while i < len(s):
+        #     if s[i].isdigit(): # O(1)
+        #         start = i
+        #         while i < len(s) and s[i].isdigit():  # O(1)
+        #             i += 1
+        #         num = int(s[start:i])
+        #         if num <= prev:
+        #             return (False)
+        #         prev = num
+        #     else:
+        #         i += 1
+        # return (True)
 
 #Time: O(N)
 #Space: O(1)
+
+#second solution with set and sorting
+        l = [int(x) for x in s.split(" ") if x.isalpha()==False]
+        return (l == sorted(l) and len(set(l)) == len(l))
+
+#Time: O(N log N)
+#Space: O(N)
