f9fab95455016deadaa0338b9d48f7d53803ed26
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2021-11-26 10:23:10 -0800

    Time: 71 ms (20.35%), Space: 14.1 MB (92.97%) - LeetHub

--- time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
+++ time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
@@ -1,20 +1,20 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        # res = 0
-        # while tickets[k] != 0:
-        #     for i in range(len(tickets)):
-        #         if tickets[i] > 0:
-        #             tickets[i] -= 1
-        #             res += 1
-        #             # print(tickets)
-        #         if tickets[k] == 0:
-        #             break
-        # return res
+        res = 0
+        while tickets[k] != 0:
+            for i in range(len(tickets)):
+                if tickets[i] > 0:
+                    tickets[i] -= 1
+                    res += 1
+                    # print(tickets)
+                if tickets[k] == 0:
+                    break
+        return res
 
 # Time: O(N * T) // T = max(tickets[i])
 # Space: O(1)
 
 
-        return (sum( [ min(tickets[i], tickets[k] if k >= i else tickets[k]-1) for i in range(len(tickets))] ))
+        # return (sum( [ min(tickets[i], tickets[k] if k >= i else tickets[k]-1) for i in range(len(tickets))] ))
 # Time: O(N)
 # Space: O(N)
