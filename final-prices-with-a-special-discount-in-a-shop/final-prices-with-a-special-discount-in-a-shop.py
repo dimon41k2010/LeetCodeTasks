457358f8702c62eb23ce899299fb0a4b4a0a2c05
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2021-11-26 07:39:57 -0800

    Time: 74 ms (20.66%), Space: 14.2 MB (94.79%) - LeetHub

--- final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
+++ final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
@@ -1,18 +1,33 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
-        res = []
+#         res = []
 
-        for i in range(len(prices)):
-            is_found = False
-            for j in range(i+1, len(prices)):
-                if prices[i] >= prices[j] and is_found == False:
-                    res.append(prices[i]-prices[j])
-                    is_found = True
-                    break
-            if is_found == False:
-                res.append(prices[i])
+#         for i in range(len(prices)):
+#             is_found = False
+#             for j in range(i+1, len(prices)):
+#                 if prices[i] >= prices[j] and is_found == False:
+#                     res.append(prices[i]-prices[j])
+#                     is_found = True
+#                     break
+#             if is_found == False:
+#                 res.append(prices[i])
 
-        return res
+#         return res
 
 # Time: O(N^2)
+# Space: O(1)
+
+# ================
+## Second APPROACH : STACK ##
+## 1. Use Monotonic Increasing Stack Concept
+## 2. Main idea is to find the Next Smaller Element in O(N) using #1
+        stack = []
+        for i, num in enumerate(prices):
+            while(stack and prices[stack[-1]] >= num):
+                popIndex = stack.pop()
+                prices[popIndex] -= num
+            stack.append(i)
+        return (prices)
+
+# Time: O(N)
 # Space: O(1)
