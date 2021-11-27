b0a8a0beff4c19325a956260bfcf39dcf0f887c2
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2021-11-27 07:26:08 -0800

    Time: 61 ms (30.92%), Space: 14.3 MB (7.69%) - LeetHub

---
+++ sqrtx/sqrtx.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        left, right = 0, x+1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid > x:
+                right = mid - 1
+            elif mid * mid < x:
+                left = mid + 1
+        return (left-1)
+
+# Time: O(log N) Binary search
+# Space: O(1)
