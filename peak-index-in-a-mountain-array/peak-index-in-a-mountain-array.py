9f169ce0ec0ec8568c60e74de1003137cf24eed3
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2021-11-30 07:38:47 -0800

    Time: 76 ms (67.08%), Space: 15.6 MB (9.85%) - LeetHub

---
+++ peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        left, right = 0, len(arr)-1
+        while left <= right:
+            mid = (left + right) // 2
+            if mid == 0 or arr[mid-1] < arr[mid]:  # Green zone
+                left = mid + 1
+            elif arr[mid-1] > arr[mid]:  # Red zone
+                 right = mid - 1
+
+        return (right)
+
+# Time: O(log N) Binary search
+# Space: O(1)
