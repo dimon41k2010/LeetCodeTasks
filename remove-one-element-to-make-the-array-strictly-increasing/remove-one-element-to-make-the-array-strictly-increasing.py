62bff378e47b6a3e7c7cdf3bd90a7e038143d6a4
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2021-12-04 15:47:43 -0800

    Time: 224 ms (35.10%), Space: 14.2 MB (98.31%) - LeetHub

--- remove-one-element-to-make-the-array-strictly-increasing/remove-one-element-to-make-the-array-strictly-increasing.py
+++ remove-one-element-to-make-the-array-strictly-increasing/remove-one-element-to-make-the-array-strictly-increasing.py
@@ -1,5 +1,6 @@
 class Solution:
     def canBeIncreasing(self, nums: List[int]) -> bool:
+        
         for i in range(len(nums)):
             temp = list(nums)
             temp.pop(i)
@@ -7,3 +8,7 @@ class Solution:
                 return (True)
             
         return(False)
+    
+    
+# Time: O(N^2) -> due to temp list creation on every iteration 
+# Space: O(N + M)
