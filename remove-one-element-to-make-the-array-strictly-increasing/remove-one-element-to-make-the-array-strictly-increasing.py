383a0d8a30ea22ac1cfc62a512607ac1b1c9ff18
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2021-12-05 09:46:24 -0800

    Time: 52 ms (76.53%), Space: 14.5 MB (44.19%) - LeetHub

--- remove-one-element-to-make-the-array-strictly-increasing/remove-one-element-to-make-the-array-strictly-increasing.py
+++ remove-one-element-to-make-the-array-strictly-increasing/remove-one-element-to-make-the-array-strictly-increasing.py
@@ -1,14 +1,29 @@
 class Solution:
     def canBeIncreasing(self, nums: List[int]) -> bool:
         
-        for i in range(len(nums)):
-            temp = list(nums)
-            temp.pop(i)
-            if temp == sorted(temp) and len(temp) == len(set(temp)):
-                return (True)
+#         for i in range(len(nums)):
+#             temp = list(nums)
+#             temp.pop(i)
+#             if temp == sorted(temp) and len(temp) == len(set(temp)):
+#                 return (True)
             
-        return(False)
-    
+#         return(False)
     
 # Time: O(N^2) -> due to temp list creation on every iteration 
 # Space: O(N + M)
+
+
+# == Second Better solution
+        def is_sorted(arr):                 # Time: O(N)
+            for i in range(1, len(arr)):
+                if arr[i-1] >= arr[i]:
+                    return False
+            return(True)
+
+        for i in range(1, len(nums)):       # Time: O(N)
+            if nums[i-1] >= nums[i]:
+                return (is_sorted(nums[0:i-1] + nums[i:]) or is_sorted(nums[0:i] + nums[i+1:]))
+        return (True)
+
+# Time: O(5 * N) = O(N)
+# Space: O(N)
