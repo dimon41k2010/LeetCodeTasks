46252e4c6e0730325641e357157d2641997bb412
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2021-12-07 09:22:17 -0800

    Time: 638 ms (7.61%), Space: 14.3 MB (47.74%) - LeetHub

--- find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
+++ find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -1,23 +1,44 @@
+
+
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
         
-        lst = [x for x in range(1, n+1)]  #Time: O(N) 
-        start_index = 0
+#         lst = [x for x in range(1, n+1)]  #Time: O(N) 
+#         start_index = 0
 
-        def get_winner(lst, start_index, k):    #Time: O(N)
-            # print(lst, start_index)
+#         def get_winner(lst, start_index, k):    #Time: O(N)
+#         # print(lst, start_index)
+
+#             if len(lst) == 1:
+#                 return lst[0]
+
+#             remove_index = (start_index + k - 1) % len(lst)
+#             # print("remove_index", remove_index)
+#             lst.pop(remove_index)                   #Time: O(N) 
+#             start_index = (remove_index) % len(lst)
+#             # print("start_index", start_index)
+#             return get_winner(lst, start_index, k)
         
-            if len(lst) == 1:
-                return lst[0]
-            
-            remove_index = (start_index + k - 1) % len(lst)
-            # print("remove_index", remove_index)
-            lst.pop(remove_index)                   #Time: O(N) 
-            start_index = (remove_index) % len(lst)
-            # print("start_index", start_index)
-            return get_winner(lst, start_index, k)
-
-        return get_winner(lst, start_index, k)
-
+        
+#         return get_winner(lst, start_index, k)
+    
+    
 #Time: O(N * N) = O(N^2) 
+#Space: O(N)
+
+
+# === 2nd Solution using Queue
+
+        queue = [x for x in range(1, n+1)]  #Time: O(N)
+        # print(queue)
+        while len(queue) != 1:
+            j = 1
+            while j <= k-1:
+                queue.append(queue.pop(0))
+                # print("queue.append",queue)
+                j += 1
+            queue.pop(0)
+        return queue[0]
+
+#Time: O(N * M) // N -> n, M -> k
 #Space: O(N)
