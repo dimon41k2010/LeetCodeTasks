b939d2ba3d0a7950834cb1e37aa5cfc8c5945f99
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2021-12-09 08:49:47 -0800

    Time: 28 ms (95.73%), Space: 14.3 MB (6.91%) - LeetHub

--- find-the-highest-altitude/find-the-highest-altitude.py
+++ find-the-highest-altitude/find-the-highest-altitude.py
@@ -7,3 +7,15 @@ class Solution:
             min_gain = max(-start, min_gain)
             # print(-start)
         return(min_gain)
+    
+#Time: O(N)
+#Space: O(1)
+
+# Second solution using list
+        res_lst = [0]
+        for i in range(len(gain)):
+            res_lst.append(res_lst[-1] + gain[i])
+        return(max(res_lst))
+
+#Time: O(N)
+#Space: O(N) / N -> len(res_lst)
