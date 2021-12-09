e22d565259b76f0c6a8daaa9dffd82d96dc0c4d2
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2021-12-08 20:28:46 -0800

    Time: 56 ms (8.87%), Space: 14.1 MB (75.27%) - LeetHub

---
+++ sum-of-unique-elements/sum-of-unique-elements.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sumOfUnique(self, nums: List[int]) -> int:
+        
+        d = {}
+        for i in range(len(nums)):
+            if nums[i] not in d.keys():
+                d[nums[i]] = 1
+            else:
+                d[nums[i]] += 1
+
+        # print(d)
+
+        return(sum( [key for key in d.keys() if d[key] == 1]))
+        
+#Time: O(N) // N -> len(nums)
+#Space: O(N)
