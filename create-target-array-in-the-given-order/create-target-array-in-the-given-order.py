3718a1c515de126dab0f6d96c21a8bf6dec0dcaf
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2021-12-10 08:56:32 -0800

    Time: 36 ms (57.77%), Space: 14.4 MB (17.52%) - LeetHub

--- create-target-array-in-the-given-order/create-target-array-in-the-given-order.py
+++ create-target-array-in-the-given-order/create-target-array-in-the-given-order.py
@@ -5,3 +5,17 @@ class Solution:
         for i in range(len(nums)):
             res.insert(index[i], nums[i])
         return (res)
+    
+# Time: O(N^2)
+# Space: O(1)
+        
+    
+    #Second solution with slicing
+        r = []
+        for val, idx in enumerate(index):
+            r = r[:idx] + [nums[val]] + r[idx:]
+            return(r)
+        
+        
+# Time: O(N^2)
+# Space: O(1)
