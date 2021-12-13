a4d2517ade8dedfe9248fe8c2f1b91e6e4d12110
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2021-12-12 17:55:56 -0800

    Time: 28 ms (87.00%), Space: 14.2 MB (87.40%) - LeetHub

--- valid-parentheses/valid-parentheses.py
+++ valid-parentheses/valid-parentheses.py
@@ -1,39 +1,40 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        d = {
-            ")": "(",
-            "}": "{",
-            "]": "["
-        }
+#         d = {
+#             ")": "(",
+#             "}": "{",
+#             "]": "["
+#         }
         
-        stack = []                               # [ ]
-        for val in s:
-            if val not in d.keys():              # open parentheses
-                stack.append(val)
+#         stack = []                               # [ ]
+#         for val in s:
+#             if val not in d.keys():              # open parentheses
+#                 stack.append(val)
             
-            elif val in d.keys():                # closed parentheses
-                if not stack or stack[-1] != d[val]: # empty stack OR top of stack not equal to current val
-                    return False    
-                else:                            # everything match
-                    stack.pop()
+#             elif val in d.keys():                # closed parentheses
+#                 if not stack or stack[-1] != d[val]: # empty stack OR top of stack not equal to current val
+#                     return False    
+#                 else:                            # everything match
+#                     stack.pop()
             
-        return True if not stack else False
+#         return True if not stack else False
     #Time: O(n) 
     #Space: O(n) + O(d)
     
+# Second solution, less space 
+
+        stack = [s[0]]
+        for i in s[1:]:
+            if i == ']' and stack and stack[-1] == '[':
+                stack.pop()
+            elif i == ')' and stack and stack[-1] == '(':
+                stack.pop()
+            elif i == '}' and stack and stack[-1] == '{':
+                stack.pop()
+            else:
+                stack.append(i)
         
-#         stack = [s[0]]
-#         for i in s[1:]:
-#             if i == ']' and stack and stack[-1] == '[':
-#                 stack.pop()
-#             elif i == ')' and stack and stack[-1] == '(':
-#                 stack.pop()
-#             elif i == '}' and stack and stack[-1] == '{':
-#                 stack.pop()
-#             else:
-#                 stack.append(i)
-        
-#         return True if not stack else False
+        return True if not stack else False
     
     #Time: O(n) 
     #Space: O(n)
