ef93c03713c93ca73ca4770eea1f3a14af179fe8
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2021-12-13 09:48:52 -0800

    Time: 228 ms (5.45%), Space: 17.7 MB (61.79%) - LeetHub

--- group-anagrams/group-anagrams.py
+++ group-anagrams/group-anagrams.py
@@ -1,12 +1,44 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#         d = {}
+
+#         for s in strs:
+#             sorted_s = ''.join(sorted(s))
+
+#             if sorted_s not in d.keys():
+#                 d[sorted_s] = [s]
+#             else:
+#                 d[sorted_s].append(s)
+#         return (d.values())
+
+    
+#Time: O(N) * O(W log W) // N->len(strs), W->len(s)
+#Space: O(N * W)
+
+# 2nd solution with custom sorting
+        from collections import Counter
+
         d = {}
+        def custom_sort(s):             # Time: O(W)
+            d_sort = Counter(s)
+            # print(d_sort)
+            res = []
+            for i in range(ord("a"), ord("z")+1):
+                c = chr(i)
+                count = d_sort[c] if c in d_sort.keys() else 0
+                for _ in range(count):
+                    res.append(c)
+            return "".join(res)
+
+        # print(custom_sort("eat"))
 
         for s in strs:
-            sorted_s = ''.join(sorted(s))
-
+            sorted_s = custom_sort(s)   # Time: O(W)
             if sorted_s not in d.keys():
                 d[sorted_s] = [s]
             else:
                 d[sorted_s].append(s)
         return (d.values())
+
+#Time: O(N) * O(W)
+#Space: O(N * W)
