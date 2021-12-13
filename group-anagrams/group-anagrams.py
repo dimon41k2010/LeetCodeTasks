057bc26a32caff8fdef0be477cbac00b8c677b1c
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2021-12-13 09:49:19 -0800

    Time: 96 ms (81.63%), Space: 17.6 MB (61.79%) - LeetHub

--- group-anagrams/group-anagrams.py
+++ group-anagrams/group-anagrams.py
@@ -1,15 +1,15 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-#         d = {}
+        d = {}
 
-#         for s in strs:
-#             sorted_s = ''.join(sorted(s))
+        for s in strs:
+            sorted_s = ''.join(sorted(s))
 
-#             if sorted_s not in d.keys():
-#                 d[sorted_s] = [s]
-#             else:
-#                 d[sorted_s].append(s)
-#         return (d.values())
+            if sorted_s not in d.keys():
+                d[sorted_s] = [s]
+            else:
+                d[sorted_s].append(s)
+        return (d.values())
 
     
 #Time: O(N) * O(W log W) // N->len(strs), W->len(s)
