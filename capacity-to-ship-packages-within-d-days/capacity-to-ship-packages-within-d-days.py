2cb5cdf4ececa03870724ac4c805848f122027c4
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2021-12-13 08:27:01 -0800

    Time: 652 ms (32.36%), Space: 17.1 MB (47.88%) - LeetHub

---
+++ capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,31 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        
+        def possible_capacity(capacity):
+            single_ship = 0
+            counter = 0
+
+            for weight in weights:
+                # print("this ",single_ship, weight,counter )
+                if single_ship + weight <= capacity:
+                    single_ship += weight
+                else:
+                    counter += 1
+                    single_ship = weight
+                    if counter > days:
+                        return False
+                if counter == days:
+                    return False
+                # print(single_ship, weight,counter)
+            return True
+
+# print(possible_capacity(7))
+
+        left, right = max(weights), sum(weights)
+        while left <= right:
+            mid = (left + right) // 2
+            if possible_capacity(mid):   # if True
+                right = mid - 1
+            else:                       # if NOT True
+                left = mid + 1
+        return(left)
