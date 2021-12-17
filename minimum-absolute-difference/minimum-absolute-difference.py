0d0fe5e88d3c5a472d88668ef9c41e697fae6f66
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2021-12-17 09:25:36 -0800

    Time: 336 ms (75.11%), Space: 28.3 MB (24.51%) - LeetHub

--- minimum-absolute-difference/minimum-absolute-difference.py
+++ minimum-absolute-difference/minimum-absolute-difference.py
@@ -1,24 +1,26 @@
 class Solution:
     def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
         
-#         arr.sort()
-#         min_diff = arr[1] - arr[0]
-#         for i in range(1, len(arr)):
-#             diff = arr[i] - arr[i-1]
-#             if diff < min_diff:
-#                 min_diff = diff
-#         # print(min_diff)
+        arr.sort()
+        min_diff = arr[1] - arr[0]
+        for i in range(1, len(arr)):
+            diff = arr[i] - arr[i-1]
+            if diff < min_diff:
+                min_diff = diff
+        # print(min_diff)
 
-#         res = []
-#         for i in range(1, len(arr)):
-#             pair = arr[i] - arr[i-1]
-#             if pair == min_diff:
-#                 res.append([arr[i-1], arr[i]])
-#         return (res)
+        res = []
+        for i in range(1, len(arr)):
+            pair = arr[i] - arr[i-1]
+            if pair == min_diff:
+                res.append([arr[i-1], arr[i]])
+        return (res)
 
 #Time: O(N Log N) + O(N)
 #Space: O(1)
 
+
+# 2nd solution using recursion
         arr.sort()
         
         def find_min_diff(index, arr):
