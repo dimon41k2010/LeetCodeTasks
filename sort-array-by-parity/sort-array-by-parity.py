1efb917f088b3c12905aa181f3b4b589d7ffe6b0
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2021-12-19 08:39:36 -0800

    Time: 92 ms (23.04%), Space: 20.1 MB (24.78%) - LeetHub

--- sort-array-by-parity/sort-array-by-parity.py
+++ sort-array-by-parity/sort-array-by-parity.py
@@ -1,6 +1,7 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
         
+# ===== recursion solution
         def recur_v2(i, nums, res):
             # print("Enter",i, res)
             if i == len(nums):
@@ -20,6 +21,20 @@ class Solution:
 
         return(result_2)
 
-
 #Time: O(N)
 #Space: O(N)
+
+# Second iterative solution
+        
+        res = []
+        for i in range(len(nums)):
+            if nums[i] % 2 == 0:
+                res.append(nums[i])
+
+        for i in range(len(nums)):
+            if nums[i] % 2 == 1:
+                res.append(nums[i])
+        return (res)
+        
+#Time: O(2 * N) => O(N)
+#Space: O(1)
