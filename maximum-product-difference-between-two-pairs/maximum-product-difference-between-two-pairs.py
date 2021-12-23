2392b5821371b3617bfd800a74dfac25564392e2
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2021-12-23 08:58:46 -0800

    Time: 176 ms (32.02%), Space: 15.6 MB (14.00%) - LeetHub

--- maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.py
+++ maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.py
@@ -29,5 +29,54 @@ class Solution:
         
         return (num_max * num_max_second) - (num_min * num_min_second)
 
+# #Time: O(N) + O(N) => O(N)
+# #Space: O(1)
+
+
+# recursion
+        def find_max(index, nums):
+            if index > len(nums)-1:
+                return 0,0
+
+            n_max, n_max_second = find_max(index+1, nums)
+            num = nums[index]
+
+            if n_max == 0:
+                n_max = num
+            elif num > n_max:
+                n_max_second = n_max
+                n_max = num
+            elif n_max_second == 0:
+                n_max_second = num
+            elif num > n_max_second:
+                n_max_second = num
+
+            return n_max, n_max_second
+
+        n_max, n_max_second = find_max(0, nums)
+
+        def find_min(index, nums):
+            if index > len(nums)-1:
+                return 0,0
+
+            n_min, n_min_second = find_min(index+1, nums)
+            num = nums[index]
+
+            if n_min == 0:
+                n_min = num
+            elif num < n_min:
+                n_min_second = n_min
+                n_min = num
+            elif n_min_second == 0:
+                n_min_second = num
+            elif num < n_min_second:
+                n_min_second = num
+
+            return n_min, n_min_second
+
+        n_min, n_min_second = find_min(0, nums)
+
+        return ( n_max * n_max_second) - (n_min * n_min_second)
+    
 #Time: O(N) + O(N) => O(N)
-#Space: O(1)
+#Space: O(N) + O(N) => O(N)
