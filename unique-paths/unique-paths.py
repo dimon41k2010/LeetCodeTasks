d82819824b918c409615a425d43a49fbcbcb3df5
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2021-12-29 08:32:08 -0800

    Time: 66 ms (5.16%), Space: 14.7 MB (5.94%) - LeetHub

---
+++ unique-paths/unique-paths.py
@@ -0,0 +1,22 @@
+class Solution:
+    def uniquePaths(self, R: int, C: int) -> int:
+        
+        dp = [ [ 0 for c in range(C) ] for r in range(R) ]
+        # print(dp)
+        dp[-1][-1] = 1
+        # print(dp)
+
+        def solve_recur(r,c):
+
+            if r >= R or c >= C:
+                return 0
+            # print("ENTER:", r,c, "|",dp[r][c])
+            if dp[r][c] != 0:
+                # print("exit memo:", r,c, "|",dp[r][c])
+                return dp[r][c]
+
+            dp[r][c] = solve_recur(r+1, c) + solve_recur(r, c+1)
+            # print("Exit:", r,c, dp[r][c])
+            return dp[r][c]
+
+        return (solve_recur(0,0))
