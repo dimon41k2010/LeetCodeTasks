f6c6f0b485014f101a9aaaa7e1d6646113473d1d
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2021-12-30 07:38:08 -0800

    Time: 54 ms (5.29%), Space: 14.3 MB (57.03%) - LeetHub

---
+++ house-robber-ii/house-robber-ii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        if len(nums) <= 2:
+            return(max(nums))
+        
+        def solver(nums):
+            
+            if len(nums) == 1:
+                return(nums[0])
+            
+            dp = [ 0 for _ in range(len(nums))]
+
+            dp[0] = nums[0]
+            dp[1] = max(nums[0], nums[1])
+
+            for i in range(2,len(nums)):
+                dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+
+            return dp[-1]
+
+        return (max(solver(nums[:-1]),solver(nums[1:])))
+
+# Time: O(2 * N) => O(N)
+# Space: O(N)
