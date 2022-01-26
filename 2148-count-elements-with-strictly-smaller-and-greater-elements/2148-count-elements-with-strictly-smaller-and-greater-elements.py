bd701c6d6850bae51382fef9e5404f87b68728fd
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2022-01-26 07:17:13 -0800

    Time: 61 ms (48.77%), Space: 14.4 MB (20.93%) - LeetHub

--- 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.py
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.py
@@ -1,25 +1,25 @@
 class Solution:
     def countElements(self, nums: List[int]) -> int:
         
-        res = 0
-        min_ = 100000
-        max_ = -100000
-        for num in nums:
-            if min_ > num:
-                min_ = num
-            if max_ < num:
-                max_ = num
+#         res = 0
+#         min_ = 100000
+#         max_ = -100000
+#         for num in nums:
+#             if min_ > num:
+#                 min_ = num
+#             if max_ < num:
+#                 max_ = num
 
-        for num in nums:
-            if num != min_ and num != max_:
-                res += 1
-        return (res)
+#         for num in nums:
+#             if num != min_ and num != max_:
+#                 res += 1
+#         return (res)
 
 # Time: O(N) + O(N) = O(N)
 # Space: O(1)
 
-        # used = set([min(nums), max(nums)])
-        # return (len([ num for num in nums if num not in used ]))
+        used = set([min(nums), max(nums)])
+        return (len([ num for num in nums if num not in used ]))
 
 # Time: O(N) + O(N) = O(N)
 # Space: O(N)
