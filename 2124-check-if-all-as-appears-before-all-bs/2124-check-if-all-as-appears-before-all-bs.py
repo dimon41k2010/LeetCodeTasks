535314924d45ac8bfb6d565a31aceb37af72dd98
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2022-01-28 07:09:05 -0800

    Time: 57 ms (13.06%), Space: 13.9 MB (96.81%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.py
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.py
@@ -1,12 +1,12 @@
 class Solution:
     def checkString(self, s: str) -> bool:
-        # is_b = False
-        # for char in s:
-        #     if char == "b":
-        #         is_b = True
-        #     elif is_b == True and char == "a":
-        #         return False
-        # return True
+        is_b = False
+        for char in s:
+            if char == "b":
+                is_b = True
+            elif is_b == True and char == "a":
+                return False
+        return True
     
 # Time: O(N)
 # Space: O(1)
@@ -20,6 +20,6 @@ class Solution:
 # Space: O(1)
 
 
-        return all([ not (s[i-1] == 'b' and s[i] == 'a') for i in range(1,len(s)) ])
+        # return all([ not (s[i-1] == 'b' and s[i] == 'a') for i in range(1,len(s)) ])
 # Time: O(N)
 # Space: O(N)
