2285fcc04c257c58155af9b62cd43943a9771736
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2022-01-31 08:20:11 -0800

    Time: 2181 ms (25.85%), Space: 26.7 MB (100.00%) - LeetHub

---
+++ 2145-count-the-hidden-sequences/2145-count-the-hidden-sequences.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numberOfArrays(self, differences: List[int], lower: int, upper: int) -> int:
+        sub_lower = 0
+        sub_upper = 0
+        hidden = 0
+
+        for diff in differences:
+            hidden += diff
+            if hidden > sub_upper:
+                sub_upper = hidden
+
+            if hidden < sub_lower:
+                sub_lower = hidden
+
+        sub_range = sub_upper - sub_lower
+        range_ = upper - lower
+        res = range_ - sub_range + 1
+        return res if res > 0 else 0
+
+# Time: O(N)
+# Space: O(1)
