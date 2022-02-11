ad6860dcbcf2933806ea3dd67a287d4ba83980fd
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2022-02-11 08:44:57 -0800

    Time: 256 ms (25.90%), Space: 29.5 MB (44.46%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
@@ -7,7 +7,7 @@
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
         
-        def lenght(head):
+        def lenght(head):   # Time:O(N+M)
             res = 0 
             while head:
                 res += 1
@@ -17,21 +17,25 @@ class Solution:
         lenght_a = lenght(headA)
         lenght_b = lenght(headB)
         
-        if lenght_a < lenght_b:
+        if lenght_a < lenght_b:     # Time:O(1)
             headA, headB = headB, headA
             lenght_a, lenght_b = lenght_b, lenght_a
         
         skip = lenght_a - lenght_b
-        for _ in range(skip):
+        for _ in range(skip):        # Time:O(N+M)
             headA = headA.next
         
         # print(headA)
         # print(headB)
         
-        while headA:
+        while headA:     # Time:O(N+M)
             if headA.__hash__() == headB.__hash__():
                 return headA
             
             headA = headA.next
             headB = headB.next
+            
+            
+# Time: O(N+M) // N->headA, M->headB
+# Space: O(1)
         
