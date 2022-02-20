abafb3842484f6bb276b3f2b344ed06711aa0327
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2022-02-20 09:45:12 -0800

    Time: 1379 ms (37.50%), Space: 16.2 MB (50.00%) - LeetHub

---
+++ 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.py
@@ -0,0 +1,37 @@
+class Solution:
+    def repeatLimitedString(self, s: str, repeatLimit: int) -> str:
+        count = Counter(s)
+        # print(count)
+
+        letter = "z"
+        # print(ord(letter))
+        # print(chr(ord('a')-1))
+        res = []
+        back_up_letter = None
+        while ord(letter) >= ord('a'):
+
+            if letter in count.keys():
+                is_break = False
+                is_backup = False
+                for _ in range(repeatLimit):
+                    res.append(letter)
+                    count[letter] -= 1
+                    if count[letter] == 0:
+                        count.pop(letter)
+                        is_break = True
+                    if back_up_letter:
+                        letter = back_up_letter
+                        back_up_letter = None
+                        is_backup = True
+                        is_break = True
+                    if is_break:
+                        break
+                if is_backup:
+                    continue
+                if letter in count.keys():
+                    back_up_letter = letter
+
+            letter = chr(ord(letter)-1)
+            # print(letter)
+        # print(res)
+        return (''.join(res))
