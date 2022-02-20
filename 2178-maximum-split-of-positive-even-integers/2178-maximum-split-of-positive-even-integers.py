0b617546c8f1efecbf0c8b8a8a0c4374478a5a79
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2022-02-20 09:01:54 -0800

    Time: 643 ms (50.00%), Space: 26.7 MB (83.33%) - LeetHub

--- 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.py
+++ 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.py
@@ -9,9 +9,12 @@ class Solution:
         while finalSum > 0:
             if finalSum - level <= level: 
                 res.append(finalSum)
-                finalSum = 0
+                finalSum = 0   # break
             else:
                 res.append(level)
                 finalSum -= level
                 level += 2
         return res
+    
+# Time: O(N) // between O(logN) and O(N) 
+# Space: O(1)
