3005ad372ca7ea0e21147d8f9dc2d7e85762f8cf
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2022-04-23 11:52:53 -0700

    Time: 32 ms (81.63%), Space: 14 MB (15.05%) - LeetHub

--- 2243-calculate-digit-sum-of-a-string/2243-calculate-digit-sum-of-a-string.py
+++ 2243-calculate-digit-sum-of-a-string/2243-calculate-digit-sum-of-a-string.py
@@ -2,21 +2,29 @@ class Solution:
     def digitSum(self, s: str, k: int) -> str:
         
         
-        def calc(chunk):
-            res = 0
-            for char in chunk:
-                res += int(char)
-            return str(res)
+#         def calc(chunk):
+#             res = 0
+#             for char in chunk:
+#                 res += int(char)
+#             return str(res)
 
-        while len(s) > k:
-            chunks = []
-            for i in range(0, len(s), k):
-                end = min(i+k, len(s))
-                chunks.append(calc(s[i:end]))
-            s = "".join(chunks)
+#         while len(s) > k:
+#             chunks = []
+#             for i in range(0, len(s), k):
+#                 end = min(i+k, len(s))
+#                 chunks.append(calc(s[i:end]))
+#             s = "".join(chunks)
 
-        return (s)
+#         return (s)
 
 
 # Time: O(N log N)
 # Space: O(N)
+
+        def calc(chunk):
+            return str(sum([int(n) for n in chunk]))
+
+        while len(s) > k:
+            s = "".join([calc(s[i:i+k]) for i in range(0, len(s), k)])
+        
+        return (s)
