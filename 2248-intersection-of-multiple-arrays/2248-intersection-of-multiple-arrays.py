da28c89c2a7f36ce9bfd406e1c9a1145a227e253
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2022-04-24 12:36:55 -0700

    Time: 100 ms (70.00%), Space: 14.2 MB (70.00%) - LeetHub

--- 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.py
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.py
@@ -1,14 +1,14 @@
 class Solution:
     def intersection(self, nums: List[List[int]]) -> List[int]:
         
-        sets = [set(num) for num in nums]
-        res = []
+#         sets = [set(num) for num in nums]
+#         res = []
 
-        for num in nums[0]:
-            if all([num in set_ for set_ in sets]):
-                res.append(num)
+#         for num in nums[0]:
+#             if all([num in set_ for set_ in sets]):
+#                 res.append(num)
 
-        return (sorted(res))
+#         return (sorted(res))
 
 # Time: O(N * L) // N = len(nums), L = len(nums[0])
 # Space: O(N * L)
@@ -16,16 +16,16 @@ class Solution:
 
 #======== Second method with count dict
 
-#         count_dict = {}
+        count_dict = {}
 
-#         for sub_nums in nums:
-#             for num in sub_nums:
-#                 if num not in count_dict.keys():
-#                     count_dict[num] = 1
-#                 else:
-#                     count_dict[num] += 1
+        for sub_nums in nums:
+            for num in sub_nums:
+                if num not in count_dict.keys():
+                    count_dict[num] = 1
+                else:
+                    count_dict[num] += 1
                     
-#         return ([ key for key in range(1,1001) if key in count_dict.keys() and count_dict[key]==len(nums) ])
+        return ([ key for key in range(1,1001) if key in count_dict.keys() and count_dict[key]==len(nums) ])
 
 # Time: O(N * L) // N = len(nums), L = len(nums[0])
 # Space: O(N * L)
