47e9c27f404d2daf1f5cb13d1e66e453c395ea07
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2022-05-15 10:24:00 -0700

    Time: 147 ms (28.57%), Space: 14 MB (42.86%) - LeetHub

---
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
@@ -0,0 +1,31 @@
+class Solution:
+    def removeAnagrams(self, words: List[str]) -> List[str]:
+        
+        def sorted(word):  # O(N)
+            d = {}
+            for char in word:
+                if char not in d.keys():
+                    d[char] = 1
+                else:
+                    d[char] += 1
+            res = []
+            for i in range(ord("a"), ord("z")+1):
+                c = chr(i)
+                if c in d.keys():
+                    res.append(c * d[c])
+
+            return ''.join(res)
+
+        
+        set_ = set()
+        res = []
+        for word in words:
+            sorted_word = sorted(word)
+            if sorted_word not in set_:
+                set_ = set()
+                set_.add(sorted_word)
+                res.append(word)
+        return (res)
+
+#Time:  O(N * M) / N = len(words), M = len(word)
+#Space: O(N * M) / N = len(words), M = len(word)
