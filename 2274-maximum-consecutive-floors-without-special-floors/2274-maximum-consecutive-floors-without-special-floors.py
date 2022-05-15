d7d8948c8e740a2a8895f7e1ffff5f702b42b90f
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2022-05-15 11:42:53 -0700

    Time: 882 ms (57.14%), Space: 28.7 MB (28.57%) - LeetHub

--- 2274-maximum-consecutive-floors-without-special-floors/2274-maximum-consecutive-floors-without-special-floors.py
+++ 2274-maximum-consecutive-floors-without-special-floors/2274-maximum-consecutive-floors-without-special-floors.py
@@ -1,14 +1,25 @@
 class Solution:
     def maxConsecutive(self, bottom: int, top: int, special: List[int]) -> int:
         
+#         special.append(bottom - 1)
+#         special.append(top + 1)
+#         special.sort()
+
+#         consecutive = -1
+#         for i in range(1, len(special)):
+#             consecutive = max(consecutive, special[i] - special[i-1] - 1)
+#         return (consecutive)
+
+#Time: O(N Log N) + O(N) => O(N Log N)
+#Space: O(1)
+
+
+# List comprehension
         special.append(bottom - 1)
         special.append(top + 1)
         special.sort()
 
-        consecutive = -1
-        for i in range(1, len(special)):
-            consecutive = max(consecutive, special[i] - special[i-1] - 1)
-        return (consecutive)
+        return (max([special[i] - special[i-1]- 1 for i in range(1, len(special))]))
 
 #Time: O(N Log N) + O(N) => O(N Log N)
-#Space: O(1)
+#Space: O(N)
