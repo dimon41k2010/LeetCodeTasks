0326e1d818639ef1db9c27d85c5b5f7f8cdfea43
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2022-05-29 11:01:55 -0700

    Time: 42 ms (71.43%), Space: 13.9 MB (57.14%) - LeetHub

---
+++ 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.py
@@ -0,0 +1,25 @@
+class Solution:
+    def rearrangeCharacters(self, s: str, target: str) -> int:
+        
+        def count_dict(s):
+            dict_s = {}
+            for char in s:
+                if char not in dict_s.keys():
+                    dict_s[char] = 1
+                else:
+                    dict_s[char] += 1
+            return dict_s
+
+        count_s = count_dict(s)
+        count_target = count_dict(target)
+        res = len(s)
+        for key in count_target.keys():
+            if key in count_s.keys():
+                res = min(res, count_s[key] // count_target[key])
+            else:
+                return 0
+        return (res)
+
+#Time: O(N)
+#Space: O(N)
+    
