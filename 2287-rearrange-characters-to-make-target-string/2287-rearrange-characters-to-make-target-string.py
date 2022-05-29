a2cc34005a83f1dc3ed4c1c73bd570fc5dc54b96
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2022-05-29 11:09:02 -0700

    Time: 61 ms (42.86%), Space: 14 MB (14.29%) - LeetHub

--- 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.py
+++ 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.py
@@ -12,13 +12,15 @@ class Solution:
 
         count_s = count_dict(s)
         count_target = count_dict(target)
-        res = len(s)
-        for key in count_target.keys():
-            if key in count_s.keys():
-                res = min(res, count_s[key] // count_target[key])
-            else:
-                return 0
-        return (res)
+        # res = len(s)
+        # for key in count_target.keys():
+        #     if key in count_s.keys():
+        #         res = min(res, count_s[key] // count_target[key])
+        #     else:
+        #         return 0
+        # return (res)
+    
+        return min([0 if key not in count_s.keys() else count_s[key] // count_target[key] for key in count_target.keys()])
 
 #Time: O(N)
 #Space: O(N)
