0383655caaf64bccddf5fdca2cc936aaf7fd12ec
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2022-05-29 11:11:17 -0700

    Time: 72 ms (28.57%), Space: 13.9 MB (14.29%) - LeetHub

--- 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.py
+++ 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.py
@@ -12,16 +12,34 @@ class Solution:
 
         count_s = count_dict(s)
         count_target = count_dict(target)
-        # res = len(s)
-        # for key in count_target.keys():
-        #     if key in count_s.keys():
-        #         res = min(res, count_s[key] // count_target[key])
-        #     else:
-        #         return 0
-        # return (res)
+        res = len(s)
+        for key in count_target.keys():
+            if key in count_s.keys():
+                res = min(res, count_s[key] // count_target[key])
+            else:
+                return 0
+        return (res)
     
-        return min([0 if key not in count_s.keys() else count_s[key] // count_target[key] for key in count_target.keys()])
+    
+# list comprehension
 
+#         def count_dict(s):
+#             dict_s = {}
+#             for char in s:
+#                 if char not in dict_s.keys():
+#                     dict_s[char] = 1
+#                 else:
+#                     dict_s[char] += 1
+#             return dict_s
+
+#         count_s = count_dict(s)
+#         count_target = count_dict(target)
+        
+#         return min([0 if key not in count_s.keys() else count_s[key] // count_target[key] for key in count_target.keys()])
+
+    
 #Time: O(N)
 #Space: O(N)
+
+
     
