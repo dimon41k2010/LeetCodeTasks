0d60c383b2b2b1db9f76cb73d3768af503626c41
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2022-07-03 14:03:15 -0700

    Time: 78 ms (88.57%), Space: 14.5 MB (7.30%) - LeetHub

--- 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.py
+++ 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.py
@@ -1,13 +1,12 @@
 class Solution:
     def countWords(self, words1: List[str], words2: List[str]) -> int:
         
-        dic = {}
+        dic2 = {}
         for word in words2:
-            if word not in dic.keys():
-                dic[word] = 1
+            if word not in dic2.keys():
+                dic2[word] = 1
             else:
-                dic[word] += 1
-        # print(dic)
+                dic2[word] += 1
 
         dic1 = {}
         for word in words1:
@@ -15,12 +14,14 @@ class Solution:
                 dic1[word] = 1
             else:
                 dic1[word] += 1
-        # print(dic1)
+
         res = 0
         for key in dic1.keys():
-            if dic1[key] == 1:
-                if key in dic.keys():
-                    if dic[key] == 1:
-                        res += 1
-        return(res)
+            if dic1[key] == 1 and key in dic2.keys():
+                if dic2[key] == 1:
+                    res += 1
+        return (res)
+
+#Time: O(N) + O(M)
+#Space: O(N) + O(M)
     
