5aed7d655d3065c845222c9c33a889b7f7859b1b
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2022-07-03 13:03:47 -0700

    Time: 57 ms (37.07%), Space: 13.9 MB (61.87%) - LeetHub

--- 2315-count-asterisks/2315-count-asterisks.py
+++ 2315-count-asterisks/2315-count-asterisks.py
@@ -1,11 +1,23 @@
 class Solution:
     def countAsterisks(self, s: str) -> int:
         
-        n_s = s.split("|")
-        count = 0
-        for i in range(0, len(n_s), 2):
-            count += n_s[i].count("*")
-        return (count)
+        # n_s = s.split("|")
+        # count = 0
+        # for i in range(0, len(n_s), 2):
+        #     count += n_s[i].count("*")
+        # return (count)
     
 #Time: O(N)
 #Space: O(N)
+
+        res = 0 
+        count_vertical_bar = 0 
+        for char in s:
+            if char == "|":
+                count_vertical_bar += 1
+            elif char == "*" and count_vertical_bar % 2 == 0:
+                res += 1
+        return res
+    
+#Time: O(N)
+#Space: O(1)
