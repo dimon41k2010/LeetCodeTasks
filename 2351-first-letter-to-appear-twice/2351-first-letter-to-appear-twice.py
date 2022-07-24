4653559b1a9f7efc1bc3f83edfa738a0a937bf3d
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2022-07-24 10:23:57 -0700

    Time: 53 ms (40.00%), Space: 13.8 MB (100.00%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.py
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.py
@@ -1,19 +1,19 @@
 class Solution:
     def repeatedCharacter(self, s: str) -> str:
         
-        # dic = {}
-        # for char in s:
-        #     if char not in dic.keys():
-        #         dic[char] = 1
-        #     else:
-        #         return(char)
-
-        set_ = set()
+        dic = {}
         for char in s:
-            if char not in set_:
-                set_.add(char)
+            if char not in dic.keys():
+                dic[char] = 1
             else:
                 return(char)
+
+        # set_ = set()
+        # for char in s:
+        #     if char not in set_:
+        #         set_.add(char)
+        #     else:
+        #         return(char)
 
 #Time: O(N)
 #Space: O(1)
