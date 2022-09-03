6a35cfaf81c0e351789f1f58b23c9da53be2cd11
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2022-09-03 11:44:31 -0700

    Time: 71 ms (60.00%), Space: 14.2 MB (20.00%) - LeetHub

---
+++ 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findSubarrays(self, nums: List[int]) -> bool:
+        sub_arr_dic = {}
+        sum_sub = 0
+        for i in range(0,len(nums)-1):
+            sum_sub = nums[i] + nums[i+1]
+            if sum_sub not in sub_arr_dic.keys():
+                sub_arr_dic[sum_sub] = i
+            else:
+                return(True)
+        return (False)
