5b724136d92d03ab454c4912df63c1e1503813b6
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2022-09-25 11:12:37 -0700

    Time: 117 ms (100.00%), Space: 14.4 MB (91.67%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,12 +1,24 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        n = 100000
-        res = []
-        dic = {}
-        for i in range(len(heights)):
-            dic[heights[i]] = names[i]
+#         n = 100000
+#         res = []
+#         dic = {}
+#         for i in range(len(heights)):
+#             dic[heights[i]] = names[i]
 
-        for num in range(n,0,-1):
-            if num in dic.keys():
-                res.append(dic[num])
-        return (res)
+#         for num in range(n,0,-1):
+#             if num in dic.keys():
+#                 res.append(dic[num])
+#         return (res)
+    
+    
+    
+        new_heights = sorted(heights, reverse=True)
+        new_res = []
+        dic_new = {}
+        for i in range(len(heights)):
+            dic_new[heights[i]] = names[i]
+
+        for val in new_heights:
+            new_res.append(dic_new[val])
+        return (new_res)
