dbb77eea32ce852d55b3a05cd34a912e0c1f71f9
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2022-10-08 12:40:46 -0700

    Time: 451 ms (24.91%), Space: 13.9 MB (24.55%) - LeetHub

--- 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.py
+++ 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.py
@@ -1,21 +1,55 @@
 class Solution:
     def countVowelSubstrings(self, word: str) -> int:
         
-        count = 0 
-        current = set() 
-        for i in range(len(word)):
-            if word[i] in 'aeiou':
-                current.add(word[i])
+#         count = 0 
+#         current = set() 
+#         for i in range(len(word)):
+#             if word[i] in 'aeiou':
+#                 current.add(word[i])
                 
-                for j in range(i+1, len(word)): 
-                    if word[j] in 'aeiou':
-                        current.add(word[j])
-                    else:
-                        break
+#                 for j in range(i+1, len(word)): 
+#                     if word[j] in 'aeiou':
+#                         current.add(word[j])
+#                     else:
+#                         break
                     
-                    if len(current) == 5:
-                        count += 1
+#                     if len(current) == 5:
+#                         count += 1
 						
-            current = set()
+#             current = set()
                         
-        return count
+#         return count
+    
+#Time: O(N)
+#Space: O(1)
+
+# second solution
+        unique_vowels = set(['a', 'e', 'i', 'o', 'u'])
+
+        def is_valid(count_dict):
+            for vowel in unique_vowels:
+                if vowel not in count_dict.keys():
+                    return False
+            return True
+
+        def add_to_counter(count_dict, key):
+            if key in count_dict.keys():
+                count_dict[key] += 1
+            else:
+                count_dict[key] = 1
+        res = 0
+
+        for i in range(len(word)):
+            count_dict = {}
+            while not is_valid(count_dict) and i < len(word) and word[i] in unique_vowels:
+                add_to_counter(count_dict, word[i])
+                i += 1
+            while is_valid(count_dict) and i < len(word) and word[i] in unique_vowels:
+                res += 1
+                i += 1
+            if is_valid(count_dict):
+                res += 1
+        return (res)
+
+#Time: O(N)
+#Space: O(1)
