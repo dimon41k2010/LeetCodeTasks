5cebdfc80ba29c386779bbbf7e3e3dbf9e4d639e
Dmytro G <37424372+dimon41k2010@users.noreply.github.com>
2023-08-05 13:14:38 -0700

    Time: 125 ms (56.46%), Space: 16.5 MB (95.19%) - LeetHub

---
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.py
@@ -0,0 +1,32 @@
+class Solution:
+    def countCompleteSubarrays(self, nums: List[int]) -> int:
+        
+        distinct = len(set(nums))
+
+        def add_to_dic(d, key):
+            if key in d.keys():
+                d[key] += 1
+            else:
+                d[key] = 1
+
+        def remove_from_dic(d, key):
+            d[key] -= 1
+            if d[key] == 0:
+                d.pop(key)
+
+        d = {}
+        left = right = 0
+        res = 0
+
+        while right < len(nums) or left < right:
+            while len(d.keys()) != distinct and right < len(nums):
+                add_to_dic(d, nums[right])
+                right += 1
+                # print(res, left, right, len(d.keys()))
+
+            if len(d.keys()) == distinct:
+                res += len(nums) - right+1
+            remove_from_dic(d, nums[left])
+            left += 1
+
+        return (res)
